14db0de13446d6a8fdb0f3a1b5f2332a520282ed
Evan PENSEC <168765795+Evan-Pensec@users.noreply.github.com>
2025-04-07 10:27:40 +0200

    hbf

--- index.php
+++ index.php
@@ -3,34 +3,18 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>gabou quiz</title>
+        <title>Gabou quiz</title>
         <meta name="description" content="">
         <link rel="stylesheet" href="styles/styles.css">
     </head>
     <body>
         <div id="contener">
-<<<<<<< HEAD
             <a class="button" id="connexion" href="">Connexion</a><br>
             <h1>Bienvenue</h1><br>
             <a href="">Addition</a><br>
             <a href="">Soustraction</a><br>
             <a href="">Multiplication</a><br>
             <a href="">Division</a><br>
-=======
-            <a class="button" id="connexion" href="">Connexion</a>
-            <h1>Bienvenue</h1>
-<<<<<<< Updated upstream
-            <a href="">Addition</a>
-            <a href="">Soustraction</a>
-            <a href="">Multiplication</a>
-            <a href="">Division</a>
-=======
-            <a href="">addition</a>
-            <a href="">-</a>
-            <a href="">x</a>
-            <a href="">%</a>
->>>>>>> Stashed changes
->>>>>>> 3e7fe6aabceb3bad0672fa046237acb77ba3ef2c
         </div>
     </body>
     </html>
