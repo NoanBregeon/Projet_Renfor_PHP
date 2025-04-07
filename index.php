da4d187d288595b5d41558f0c0f430561e28d8c7
Evan PENSEC <168765795+Evan-Pensec@users.noreply.github.com>
2025-04-07 12:25:01 +0200

    qzs

--- index.php
+++ index.php
@@ -18,16 +18,16 @@ session_start();
             <a class="button" id="connexion" href="page_connexion.php">Connexion</a><br>
             <h1>Bienvenue <?php // $_SESSION['nom'] ?></h1><br>
             <div>
-                <a class="button" href="quiz/quiz.php?type=addition">Addition</a>
+                <a class="button" href="quiz/quiz.php?type=addition">Addition<img src="styles/plus.png" alt="plus" title="plus"></a>
             </div>
             <div>
-                <a class="button" href="quiz/quiz.php?type=soustraction">Soustraction</a>
+                <a class="button" href="quiz/quiz.php?type=soustraction">Soustraction<img src="styles/remove.png" alt="plus" title="plus"></a>
             </div>
             <div>
-                <a class="button" href="quiz/quiz.php?type=multiplication">Multiplication</a>
+                <a class="button" href="quiz/quiz.php?type=multiplication">Multiplication<img src="styles/multiplication.png" alt="plus" title="plus"></a>
             </div>
             <div>
-                <a class="button" href="quiz/quiz.php?type=division">Division</a>
+                <a class="button" href="quiz/quiz.php?type=division">Division<img src="styles/division.png" alt="plus" title="plus"></a>
             </div>
         </div>
     </body>
