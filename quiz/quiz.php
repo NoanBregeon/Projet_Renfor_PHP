b52d3364eb7b7fa628ec47b24a2e298540a4e43b
Evan PENSEC <168765795+Evan-Pensec@users.noreply.github.com>
2025-04-07 13:54:45 +0200

    dcqs

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -25,10 +25,10 @@ $_SESSION[$progression]=40;
                     echo '<div><a class="button bloquer moyen" href="quiz.php?type='.$_GET["type"].'">moyen</a></div>';
                 }
                 if ($_SESSION[$progression] >= 40){
-                    echo '<div><a class="button difficil" href="quiz.php?type='.$_GET["type"].'&niv=3">difficile</a></div>';
+                    echo '<div><a class="button difficile" href="quiz.php?type='.$_GET["type"].'&niv=3">difficile</a></div>';
                 }else{
                     
-                    echo '<div><a class="button bloquer difficil" href="quiz.php?type='.$_GET["type"].'">difficile</a></div>';
+                    echo '<div><a class="button bloquer difficile" href="quiz.php?type='.$_GET["type"].'">difficile</a></div>';
                 }
                 
                 echo '</div>';
