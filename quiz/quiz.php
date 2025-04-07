777f59947aa296fb6cb1fbe080e3be83765f2aa2
thibault mahe <73552366+ThibaultMahe1@users.noreply.github.com>
2025-04-07 09:50:55 +0200

    Create quiz.php

---
+++ quiz/quiz.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gabou quiz</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contener">
+
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
