4eaf23e8db02296d3345f7b3b57b5f9b14af3434
Furie-Nocturne-20 <furienocturne2.0@gmail.com>
2025-04-07 16:39:40 +0200

    Update page_connexion.php

--- liaison/page_connexion.php
+++ liaison/page_connexion.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['division'] = $progression['progression_division'];
 
         // Redirection
-        header("Location: index.php");
+        header("Location: ../index.php");
         exit;
     } else {
         $message = "❌ Nom d’utilisateur ou mot de passe incorrect.";
