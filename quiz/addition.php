47cdfb173f18c9b83d5a8b00e883b95c03311748
thibault mahe <73552366+ThibaultMahe1@users.noreply.github.com>
2025-04-07 16:55:00 +0200

    Update addition.php

--- quiz/addition.php
+++ quiz/addition.php
@@ -1,6 +1,8 @@
 <?php
 $question=1;
-
+if($_GET["niv"]==1){$dificulty=0;}
+elseif($_GET["niv"]==2){$dificulty=20;}
+elseif($_GET["niv"]==3){$dificulty=40;}
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,9 +16,9 @@ $question=1;
             <?php 
             while ($question<=20){
                 echo '<div class="button';
-                if($_SESSION[$_GET["type"]]<$question){echo " bloquer";}
+                if($_SESSION[$_GET["type"]]<$question+$dificulty){echo " bloquer";}
                 echo'">';
-                if($_SESSION[$_GET["type"]]>=$question){
+                if($_SESSION[$_GET["type"]]>=$question+$dificulty){
                     echo ' <a href="quiz.php?type='.$_GET["type"].'&question=';
                     if($_GET["niv"]==1){echo $question;}
                     elseif($_GET["niv"]==2){echo $question+20;}
