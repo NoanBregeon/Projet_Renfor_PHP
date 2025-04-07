6f723c28dff4e910332128bb844e8d60072cbf58
thibault mahe <73552366+ThibaultMahe1@users.noreply.github.com>
2025-04-07 09:58:13 +0200

    Update quiz.php

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -9,11 +9,14 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gabou quiz</title>
     <meta name="description" content="">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
     <div class="contener">
+        <div class="choix du niveau">
 
+            
+        </div>
     </div>
     <script src="script.js"></script>
 </body>
