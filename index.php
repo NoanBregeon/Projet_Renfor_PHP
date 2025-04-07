16bfc069c80a7a4c81d597b0be378212e872abcb
Evan PENSEC <168765795+Evan-Pensec@users.noreply.github.com>
2025-04-07 09:58:05 +0200

    CXQS
    CQS

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>js</title>
         <meta name="description" content="">
-        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="styles/styles.css">
     </head>
     <body>
         <div id="contener">
