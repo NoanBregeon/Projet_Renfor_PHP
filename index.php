9a1fb26863a9247499a4e17ad7dbfd158b0a974a
Evan PENSEC <168765795+Evan-Pensec@users.noreply.github.com>
2025-04-07 16:36:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,27 +17,26 @@ session_start();
     <body>
         <div class="contener">
             <?php if(isset($_SESSION['nom'])): ?>
-                <a class="button" id="deconnexion" href="liaison/deconnexion.php">Déconnection</a><br>
-                
-                <h1>Bienvenue</h1><br>
-                <div>
-                    <a class="button" href="quiz/quiz.php?type=addition">Addition <img src="styles/plus.png" alt="plus" title="plus"></a>
-                </div>
-                <div>
-                    <a class="button" href="quiz/quiz.php?type=soustraction">Soustraction <img src="styles/remove.png" alt="plus" title="plus"></a>
-                </div>
-                <div>
-                    <a class="button" href="quiz/quiz.php?type=multiplication">Multiplication <img src="styles/multiplication.png" alt="plus" title="plus"></a>
-                </div>
-                <div>
-                    <a class="button" href="quiz/quiz.php?type=division">Division <img src="styles/division.png" alt="plus" title="plus"></a>
-                </div>
+            <a class="button" id="deconnexion" href="liaison/deconnexion.php">Déconnection</a><br>
+            <h1>Bienvenue</h1><br>
+            <div>
+                <a class="button" href="quiz/quiz.php?type=addition">Addition <img src="styles/plus.png" alt="plus" title="plus"></a>
+            </div>
+            <div>
+                <a class="button" href="quiz/quiz.php?type=soustraction">Soustraction <img src="styles/remove.png" alt="plus" title="plus"></a>
+            </div>
+            <div>
+                <a class="button" href="quiz/quiz.php?type=multiplication">Multiplication <img src="styles/multiplication.png" alt="plus" title="plus"></a>
+            </div>
+            <div>
+                <a class="button" href="quiz/quiz.php?type=division">Division <img src="styles/division.png" alt="plus" title="plus"></a>
+            </div>
             <?php else: ?>
                 <h1>Bienvenue</h1>
                 <div>
-                <a class="button pasco" id="connexion" href="page_connexion.php">Connexion</a>
+                    <a class="button" id="connexion" href="liaison/page_connexion.php">Connexion</a>
                 </div>
-            <?php endif; ?>
+                <?php endif; ?>
         </div>
     </body>
     </html>
