7054d588d45eaf974e8a2b4d9f146ad5ccb5048f
thibault mahe <73552366+ThibaultMahe1@users.noreply.github.com>
2025-04-10 15:56:48 +0200

    rdxtfh

--- liaison/creationSansChatGPT.php
+++ liaison/creationSansChatGPT.php
@@ -42,6 +42,7 @@ if(isset($_POST["validation"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gabou quiz</title>
+    <link rel="stylesheet" href="../styles/styles.css?v=0">
 </head>
 <body>
     <div class="header"></div>
