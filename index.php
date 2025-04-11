d0fedd9f95c44f79b9458f4204c156188ee2c819
Furie-Nocturne-20 <furienocturne2.0@gmail.com>
2025-04-11 08:59:17 +0200

    erzrhg

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ if ($percent_global == 100) $couleur = "#00c200";
             <div class="header"></div>
             <h1>Bienvenue</h1>
             <div>
-                <a class="button" id="connexion" href='liaison/connexionSansChatGPT.php'>Connexion</a><br>
+                <a class="button" id="connexion" href='liaison/connexion.php'>Connexion</a><br>
                 <a class="button" id="connexion" href='liaison/creationSansChatGPT.php'>crée un compte</a><br>
             </div>
             <div class="footer"></div>
