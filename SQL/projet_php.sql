c976909d567a0cd780e54fd56e229bc0bd2a2b9c
Furie-Nocturne-20 <furienocturne2.0@gmail.com>
2025-04-11 13:41:46 +0200

    Update projet_php.sql

--- SQL/projet_php.sql
+++ SQL/projet_php.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 09 avr. 2025 à 12:07
--- Version du serveur : 9.1.0
--- Version de PHP : 8.3.14
+-- Généré le : ven. 11 avr. 2025 à 11:41
+-- Version du serveur : 8.3.0
+-- Version de PHP : 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -297,11 +297,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `roles` enum('user','admin') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'user',
-  `answered_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `progression_addition` int DEFAULT '1',
+  `valid_addition` int DEFAULT '0',
   `progression_soustraction` int DEFAULT '1',
+  `valid_soustraction` int DEFAULT '0',
   `progression_division` int DEFAULT '1',
+  `valid_division` int DEFAULT '0',
   `progression_multiplication` int DEFAULT '1',
+  `valid_multiplication` int DEFAULT '0',
   `streak` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
@@ -311,11 +314,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `roles`, `answered_at`, `progression_addition`, `progression_soustraction`, `progression_division`, `progression_multiplication`, `streak`) VALUES
-(1, 'Admin', 'Admin1234', 'admin', '2025-04-08 08:42:25', 60, 60, 60, 60, 0),
-(2, 'TEST', 'TEST', 'user', '2025-04-08 08:42:25', 1, 1, 1, 1, 0),
-(3, 'enfant1', 'enfant1', 'user', '2025-04-08 09:49:04', 10, 8, 40, 28, 0),
-(4, 'enfant2', 'enfant2', 'user', '2025-04-08 09:51:25', 28, 5, 40, 30, 0);
+INSERT INTO `users` (`id`, `username`, `password`, `roles`, `progression_addition`, `valid_addition`, `progression_soustraction`, `valid_soustraction`, `progression_division`, `valid_division`, `progression_multiplication`, `valid_multiplication`, `streak`) VALUES
+(1, 'Admin', 'Admin1234', 'admin', 60, 60, 60, 60, 60, 60, 60, 60, 0),
+(2, 'TEST', 'TEST', 'user', 1, 0, 1, 0, 1, 0, 1, 0, 0),
+(3, 'enfant1', 'enfant1', 'user', 10, 0, 8, 0, 40, 0, 28, 0, 0),
+(4, 'enfant2', 'enfant2', 'user', 28, 0, 5, 0, 40, 0, 30, 0, 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
